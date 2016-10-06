e33b5599325648c3988c8e02c221d1b3192d9450
Petra Jordan <petra.jordan@openforce.com>
2016-10-06 09:50:55 +0200

    First Commit

---
+++ src/main/scala/lists/lists.scala
@@ -0,0 +1,16 @@
+package lists
+
+object lists {
+
+  def main(args: Array[String]): Unit = {
+
+    val defaultList = List(1, 1, 2, 3, 5, 8)
+    println("Get last element of a list:")
+    println(defaultList.last)
+
+
+
+
+
+  }
+}
