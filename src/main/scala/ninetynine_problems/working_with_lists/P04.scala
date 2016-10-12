e4a90a1c244395ac1c8985dadac5821255f53a39
Petra Jordan <petra.jordan@openforce.com>
2016-10-12 11:24:57 +0200

    finished P04

---
+++ src/main/scala/ninetynine_problems/working_with_lists/P04.scala
@@ -0,0 +1,45 @@
+package ninetynine_problems.working_with_lists
+
+/**
+  * from: http://aperiodic.net/phil/scala/s-99/
+  */
+object P04 {
+
+  def main(args: Array[String]): Unit = {
+
+    /**
+      * P04: Find the number of elements of a list.
+      * List(1, 1, 2, 3, 5, 8)
+      */
+
+    val list = List(1, 1, 2, 3, 5, 8)
+
+    /**
+      * Solution 1
+      */
+
+    println(list.size)
+    // RESULT: 6
+
+    /**
+      * Solution 2
+      */
+
+    println(list.length)
+    // RESULT: 6
+
+    /**
+      * Solution 3
+      */
+
+    def findNumberOfElementsInAList (n : Int, list: List[Int]) : Int = (n, list) match {
+      case (n, _ :: Nil) => n + 1   //if end of list is reached, return n and a last + 1
+      case (n, _ :: tail) => findNumberOfElementsInAList(n + 1, tail)
+      case (0, Nil) => 0
+      }
+
+    println(findNumberOfElementsInAList(0, list))   // n = 0 at the beginning
+    // RESULT: 6
+
+  }
+}
