36bb6c6bd95750541817571ddc65ecde17e14e70
Petra Jordan <petra.jordan@openforce.com>
2016-10-13 12:59:46 +0200

    finished P12

--- src/main/scala/ninetynine_problems/working_with_lists/P11.scala
+++ src/main/scala/ninetynine_problems/working_with_lists/P11.scala
@@ -9,7 +9,7 @@ object P11 {
   def main(args: Array[String]): Unit = {
 
     /**
-      * Modified run-length encoding.
+      * P11: Modified run-length encoding.
       * Modify the result of problem P10 in such a way that if an element has no duplicates it is simply copied
       * into the result list. Only elements with duplicates are transferred as (N, E) terms.
       * List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
---
+++ src/main/scala/ninetynine_problems/working_with_lists/P12.scala
@@ -0,0 +1,54 @@
+package ninetynine_problems.working_with_lists
+
+import scala.collection.mutable.ListBuffer
+
+/**
+  * from: http://aperiodic.net/phil/scala/s-99/
+  */
+
+object P12 {
+
+  def main(args: Array[String]): Unit = {
+
+    /**
+      * P12: Decode a run-length encoded list.
+      * Given a run-length code list generated as specified in problem P10, construct its uncompressed version.
+      * List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))
+      * should be
+      * List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+      */
+
+    val list = List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))
+
+    /**
+      * Solution 1
+      * Petra
+      */
+
+    var finalList = new ListBuffer[Symbol]  // mutable list
+
+    def flattenList(list: List[(Int, Symbol)]) : List[Symbol] = list match {
+      case Nil => finalList.toList
+      case head :: tail =>    // looks like this: head = (4,'a), tail = List((1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
+       for (i <- 1 to head._1) {
+         finalList += head._2   // number of elements in tuple will be written into finalList
+       }
+      flattenList(tail)
+      }
+    println(flattenList(list))
+
+    /**
+      * Solution 2
+      * Phil Gold
+      * can't compile Phil's solution!
+      */
+
+//    def decode[Symbol](list: List[(Int, Symbol)]): List[Symbol] = {
+//      list.flatMap {
+//        e => List.make(e._1, e._2)
+//      }
+//    }
+//    println(decode(list))
+
+  }
+}
