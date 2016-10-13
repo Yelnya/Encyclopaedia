a26c256d45405e276e619d4e11149e58acb003de
Petra Jordan <petra.jordan@openforce.com>
2016-10-13 18:12:41 +0200

    finished P21

---
+++ src/main/scala/ninetynine_problems/working_with_lists/P21.scala
@@ -0,0 +1,50 @@
+package ninetynine_problems.working_with_lists
+
+/**
+  * from: http://aperiodic.net/phil/scala/s-99/
+  */
+object P21 {
+
+  def main(args: Array[String]): Unit = {
+
+    /**
+      * P21: Insert an element at a given position into a list.
+      * insertAt('new, 1, List('a, 'b, 'c, 'd))
+      * should look like
+      * List[Symbol] = List('a, 'new, 'b, 'c, 'd)
+      */
+
+    val list = List('a, 'b, 'c, 'd)
+    val positionToInsert = 1
+    val valueToInsert = 'new
+
+    /**
+      * Solution 1
+      * Petra
+      */
+
+    def insertValueAtPosition(list: List[Symbol], positionToInsert: Int, valueToInsert: Symbol): List[Symbol] = {
+      if (positionToInsert < list.length && positionToInsert > 0) {
+        list match {
+          case Nil => Nil
+          case head :: tail =>
+            if (positionToInsert == 0) {
+              valueToInsert :: head :: tail
+            } else {
+              head :: insertValueAtPosition(tail, positionToInsert - 1, valueToInsert)
+            }
+        }
+      } else {
+        throw new IndexOutOfBoundsException
+      }
+    }
+    println(insertValueAtPosition(list, positionToInsert, valueToInsert))
+    // RESULT: List('a, 'new, 'b, 'c, 'd)
+
+    /**
+      * Solution 2
+      * Phil Gold
+      */
+
+  }
+}
