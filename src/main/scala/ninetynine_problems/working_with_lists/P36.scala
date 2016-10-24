c9119c0c38d3b7ea524cbbdd45e3001b56fc4bd6
Petra Jordan <petra.jordan@openforce.com>
2016-10-24 17:50:21 +0200

    finished P36 complete

--- src/main/scala/ninetynine_problems/working_with_lists/P36.scala
+++ src/main/scala/ninetynine_problems/working_with_lists/P36.scala
@@ -75,9 +75,46 @@ object P36 {
     val counts = primeFactorsList.groupBy(w => w).mapValues(_.size)
     println(counts)
     // RESULT: Map(5 -> 1, 7 -> 1, 3 -> 2)
-    val countsSorted = ListMap(counts.toSeq.sortBy(_._1):_*)
+    val countsSorted = ListMap(counts.toSeq.sortBy(_._1): _*)
     println(countsSorted)
     // RESULT: Map(3 -> 2, 5 -> 1, 7 -> 1)
 
+
+    /**
+      * Solution 2
+      * Phil Gold
+      * BUT does not compile!
+      */
+
+    //    def isPrime: Boolean =
+    //    (value > 1) && (primes takeWhile {
+    //      _ <= Math.sqrt(value)
+    //    } forall {
+    //      value % _ != 0
+    //    })
+    //  }
+    //
+    //  val primes = Stream.cons(2, Stream.from(3, 2) filter {
+    //    _.isPrime
+    //  })
+    //
+    //  def primeFactorMultiplicity: Map[Int, Int] = {
+    //    def factorCount(n: Int, p: Int): (Int, Int) =
+    //      if (n % p != 0) (0, n)
+    //      else factorCount(n / p, p) match {
+    //        case (c, d) => (c + 1, d)
+    //      }
+    //    def factorsR(n: Int, ps: Stream[Int]): Map[Int, Int] =
+    //      if (n == 1) Map()
+    //      else if (n.isPrime) Map(n -> 1)
+    //      else {
+    //        val nps = ps.dropWhile(n % _ != 0)
+    //        val (count, dividend) = factorCount(n, nps.head)
+    //        Map(nps.head -> count) ++ factorsR(dividend, nps.tail)
+    //      }
+    //    factorsR(value, primes)
+    //  }
+
   }
+
 }
